6267b60b80e99feb0729f330df1f45e07fd75f7a
Titus Murdock <titusjmurdock@gmail.com>
2023-11-07 16:04:10 -0500

    after meeting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#createing scene object
+#creating scene object
 
 class Scene:
     def __init__(self, description, interactables):
@@ -16,13 +16,46 @@ class Interactable:
     def describe(self):
         print(self.description)
 
+class Enemy:
+    def __init__(self, description, hp, damage):
+        self.description = description
+        self.hp = hp
+        self.damage = damage
+    
+    def do_damage(self, Player):
+        Player.hp = Player.hp - self.damage
+
+class Player(Enemy):
+    def __init__(self, description, hp, damage, inventory,):
+        super().__init__(description, hp, damage)
+        self.inventory = inventory
+        for i in inventory:
+            if inventory[i] == "machete":
+                self.damage = 3
+            else:
+                self.damage = damage
+
+
+def start_menu():
+    choice = input("Would you like to go on an adventure? (yes or no)").lower
+    if choice == "yes":
+        return True
+    else:
+        return False
+
+
+
+
 
 if __name__ == '__main__':
+    running = True
+    running = start_menu()
+    while running == True:
+        door = Interactable("a simple door, it could be locked", ["key", "hammer"])
+        bush = Interactable("just a bush", ["machete", "match"])
 
-    door = Interactable("a simple door, it could be locked", ["key", "hammer"])
-    bush = Interactable("just a bush", ["machete", "match"])
 
-
-    scene1 = Scene("a test scene", [door, bush])
-    scene1.describe()
-    print(scene1.interactables)
+        scene1 = Scene("a test scene", [door, bush])
+        
+        player = Player("the player", 10, 1, ["key", "other thing"] )
+        print("running")
