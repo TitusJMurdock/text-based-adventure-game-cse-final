200bb0c27d9a048044b417325a30926b51493267
Titus Murdock <titusjmurdock@gmail.com>
2023-11-10 01:20:31 -0500

    added hp and damage system w/ variable damage

--- main.py
+++ main.py
@@ -21,41 +21,60 @@ class Enemy:
         self.description = description
         self.hp = hp
         self.damage = damage
+        self.alive = True
+        
     
     def do_damage(self, Player):
         Player.hp = Player.hp - self.damage
+    
+    def update(self):
+        if self.hp == 0:
+            self.alive = False
+
 
 class Player(Enemy):
     def __init__(self, description, hp, damage, inventory,):
         super().__init__(description, hp, damage)
         self.inventory = inventory
-        for i in inventory:
-            if inventory[i] == "machete":
-                self.damage = 3
-            else:
-                self.damage = damage
+        
+
+    def update(self):
+        if self.hp == 0:
+            self.alive = False
+
+        if "machete" in self.inventory:
+            self.damage = 3
 
 
 def start_menu():
-    choice = input("Would you like to go on an adventure? (yes or no)").lower
+    choice = (input("Would you like to go on an adventure? (yes or no) ")).lower()
+
     if choice == "yes":
         return True
-    else:
+    elif choice == "no":
         return False
+    else:
+        print("not a valid input")
 
 
 
 
 
 if __name__ == '__main__':
-    running = True
-    running = start_menu()
+    running = False
+    door = Interactable("a simple door, it could be locked", ["key", "hammer"])
+    bush = Interactable("just a bush", ["machete", "match"])
+
+
+    scene1 = Scene("a test scene", [door, bush])
     while running == True:
-        door = Interactable("a simple door, it could be locked", ["key", "hammer"])
-        bush = Interactable("just a bush", ["machete", "match"])
-
-
-        scene1 = Scene("a test scene", [door, bush])
+        pass
         
-        player = Player("the player", 10, 1, ["key", "other thing"] )
-        print("running")
+    player = Player("description", 10, 1, ["key", "something"])
+    python = Enemy("a scary snake", 5, 10)
+    print(player.hp)
+    print(player.alive)
+    python.do_damage(player)
+    player.update()
+    print(player.hp)
+    print(player.alive)
